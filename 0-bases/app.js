d7143bdaf9ed9c56ba9890c694943982aa5b9093
LuisFarfan57 <luise.prado99@hotmail.com>
2023-07-08 16:40:38 -0600

    feat: bases

---
+++ 0-bases/app.js
@@ -0,0 +1,5 @@
+"use strict";
+let msg = "Hello World";
+console.log(msg);
+let num = 10;
+num = 20;
---
+++ 0-bases/app.ts
@@ -0,0 +1,7 @@
+let msg: string = "Hello World"
+
+console.log(msg)
+
+let num: number = 10
+
+num = 20
